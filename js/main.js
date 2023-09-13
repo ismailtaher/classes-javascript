aff31beba516039eb9c6fa24efbb4522a979279c
ismailtaher <ismail.taher@ymail.com>
2023-09-13 20:28:30 +0500

    3rd commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ class Pizza {
     this.type = pizzaType;
     this.size = pizzaSize;
     this.crust = "original";
+    this.toppings = [];
   }
   getCrust() {
     return this.crust; // Here we return the value of crust
@@ -12,13 +13,23 @@ class Pizza {
   setCrust(pizzaCrust) {
     this.crust = pizzaCrust; // here we set the value of crust
   }
+  getToppings() {
+    return this.toppings;
+  }
+  setToppings(toppings) {
+    this.toppings.push(toppings);
+  }
   bake() {
-    console.log(`Baking a ${this.size} ${this.type} ${this.crust} crust pizza`);
+    console.log(
+      `Baking a ${this.size} ${this.type} ${this.crust} crust pizza with ${this.toppings[0]} and ${this.toppings[1]}`
+    );
   }
 }
 
 const myPizza = new Pizza("pepperoni", "small");
 /* myPizza.type = "supreme"; */ // not desirable to access properties like that
 myPizza.setCrust("thin"); // here we set the value of crust to thin using setCrust()
+myPizza.setToppings("sausage");
+myPizza.setToppings("olives");
 myPizza.bake();
-console.log(myPizza.getCrust()); // here we are returning the value of crust using getCrust()
+console.log(myPizza.getToppings()); // here we are returning the value of crust using getCrust()
